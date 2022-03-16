5625369228d13ececf7a15d8788a58c0dbfda0ee
ljy6816@gmail.com <ljy6816@gmail.com>
2022-03-17 01:21:50 +0900

    update todo input, list

--- lv2/vue-todo/src/components/TodoList.vue
+++ lv2/vue-todo/src/components/TodoList.vue
@@ -1,15 +1,85 @@
 <template>
   <div>
-      list
+    <ul>
+      <li v-for="(todoItem, idx) in todoItems" v-bind:key="todoItem" class="shadow">
+        {{ todoItem }}
+        <span class="removeBtn" v-on:click="removeItem(todoItem, idx)">
+          <i class="removeBtn fas fa-trash-alt"></i>
+        </span>
+      </li>
+      <!-- <button class="checkBtn" v-on:click=""></button>
+      <button class="checkBtnCompleted"></button>
+      <button class="textCompleted"></button> -->
+    </ul>
   </div>
 </template>
 
 <script>
 export default {
-
+  data() {
+    return {
+      todoItems: []
+    }
+  },
+  created: function() {
+    console.log('created')
+    if (localStorage.length > 0) {
+      for (var i = 0; i < localStorage.length; i++) {
+        if (localStorage.key(i) !== 'loglevel:webpack-dev-server') {
+          this.todoItems.push(localStorage.key(i))
+        }
+      }
+    }
+  },
+  methods: {
+    removeItem: function(item, key) {
+      console.log('remove items')
+      localStorage.removeItem(item)
+      this.todoItems.splice(key, 1);
+    }
+  }
 }
 </script>
 
-<style>
-
+<style scoped>
+ul {
+  list-style-type: none;
+  padding-left: 0px;
+  margin-top: 0;
+  text-align: left;
+}
+li {
+  display: flex;
+  min-height: 50px;
+  height: 50px;
+  line-height: 50px;
+  margin: 0.5rem 0;
+  padding: 0 0.9rem;
+  background: white;
+  border-radius: 5px;
+}
+.checkBtn {
+  line-height: 45px;
+  color: #62acde;
+  margin-right: 5px;
+}
+.checkBtnCompleted {
+  color: #b3adad;
+}
+.textCompleted {
+  text-decoration: line-through;
+  color: #b3adad;
+}
+.removeBtn {
+  margin-left: auto;
+  color: #de4343;
+}
+/* transition css */
+.list-enter-active, .list-leave-active {
+  transition: all 1s;
+}
+.list-enter, .list-leave-to /* .list-leave-active below version 2.1.8 */ {
+  opacity: 0;
+  transform: translateY(30px);
+}
 </style>
