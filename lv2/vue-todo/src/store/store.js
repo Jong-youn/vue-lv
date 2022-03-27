fcfb5e37750b380caeb4d92ac1344b29a9867923
ljy6816@gmail.com <ljy6816@gmail.com>
2022-03-27 23:59:54 +0900

    modulize store

--- lv2/vue-todo/src/store/store.js
+++ lv2/vue-todo/src/store/store.js
@@ -1,30 +1,11 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import todoApp from './modules/todoApp'
 
 Vue.use(Vuex);
 
-const storage = {
-  fetch() {
-    const arr = [];
-    if (localStorage.length > 0) {
-      for (var i = 0; i < localStorage.length; i++) {
-        if (localStorage.key(i) !== 'loglevel:webpack-dev-server') {
-          arr.push(JSON.parse(localStorage.getItem(localStorage.key(i))))
-        }
-      }
-    }
-    console.log(arr)
-    return arr;
-  },
-}
-
-import * as getters from './getters'
-import * as mutations from './mutations'
-
 export const store = new Vuex.Store({
-  state: {
-    todoItems: storage.fetch()
-  },
-  getters,
-  mutations
+  modules: {
+    todoApp
+  }
 });
