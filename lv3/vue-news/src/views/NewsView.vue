5499ecbfca0eb8db051576ca06a4aa95b10a46e5
ljy6816@gmail.com <ljy6816@gmail.com>
2022-04-05 17:51:02 +0900

    set routers

--- lv3/vue-news/src/views/NewsView.vue
+++ lv3/vue-news/src/views/NewsView.vue
@@ -1,12 +1,27 @@
 <template>
   <div>
-    news
+    
   </div>
 </template>
 
 <script>
-export default {
+import axios from 'axios';
 
+export default {
+  data() {
+    return {
+      users: []
+    }
+  },
+  created() {
+    axios.get('https://api.hnpwa.com/v0/news/1.json')
+      .then(function(response) {
+        console.log(response)
+      })
+      .catch(function(error) {
+        console.log(error)
+      })
+  }
 }
 </script>
 
