9716bd007aa4726bf48ddd2d6618da45869f7315
ljy6816@gmail.com <ljy6816@gmail.com>
2022-04-08 14:09:52 +0900

    styling

--- lv3/vue-news/src/views/NewsView.vue
+++ lv3/vue-news/src/views/NewsView.vue
@@ -1,6 +1,9 @@
 <template>
   <div>
-    <div v-for="news in fetchedNews" v-bind:key=news.id>{{ news.title }}</div>
+    <p v-for="news in fetchedNews" v-bind:key=news.id>
+      <a v-bind:href="news.url">{{ news.title }}</a>
+      <small>{{ news.time_ago }} by {{ news.user }}</small>
+    </p>
   </div>
 </template>
 
